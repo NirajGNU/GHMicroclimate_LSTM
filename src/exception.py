86c7357c18989141290dc0c1eee51c2a5194e67b
Niraj <niraj@gnu.ac.kr>
2025-02-01 12:27:26 +0900

    Data ingestion and Train and Test Split Done

--- src/exception.py
+++ src/exception.py
@@ -16,11 +16,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == '__main__':
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info(e)
-        raise CustomException(e, sys)
+# if __name__ == '__main__':
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info(e)
+#         raise CustomException(e, sys)
         
        
